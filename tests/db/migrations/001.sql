6398ec1c2c65c4f73400f05e1cc1e894a687f78c
Joe Fallon <joseph.t.fallon@gmail.com>
2015-12-31 22:11:25 -0800

    The schema was updated.

--- tests/db/migrations/001.sql
+++ tests/db/migrations/001.sql
@@ -1,33 +1,28 @@
--- phpMyAdmin SQL Dump
--- version 3.3.10.5
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.7.9, for osx10.9 (x86_64)
 --
--- Host: localhost
--- Generation Time: Dec 27, 2015 at 03:00 PM
--- Server version: 5.5.46
--- PHP Version: 7.0.1-2+deb.sury.org~trusty+1
-
-SET FOREIGN_KEY_CHECKS=0;
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
+-- Host: localhost    Database: phpdatabase_test
+-- ------------------------------------------------------
+-- Server version	5.5.46-0ubuntu0.14.04.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
---
--- Database: `phpdatabase_test`
---
-
--- --------------------------------------------------------
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `example_entity_table`
 --
 
 DROP TABLE IF EXISTS `example_entity_table`;
-CREATE TABLE IF NOT EXISTS `example_entity_table` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `example_entity_table` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `nullable` varchar(255) DEFAULT NULL,
@@ -35,18 +30,19 @@ CREATE TABLE IF NOT EXISTS `example_entity_table` (
   `created` datetime NOT NULL,
   `updated` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`),
-  KEY `nullable_val` (`nullable`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+  UNIQUE KEY `name_idx` (`name`),
+  KEY `nullable_val_idx` (`nullable`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `example_join_table`
 --
 
 DROP TABLE IF EXISTS `example_join_table`;
-CREATE TABLE IF NOT EXISTS `example_join_table` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `example_join_table` (
   `id1` int(11) unsigned NOT NULL,
   `id2` int(11) unsigned NOT NULL,
   `created` datetime NOT NULL,
@@ -54,4 +50,15 @@ CREATE TABLE IF NOT EXISTS `example_join_table` (
   KEY `id1_idx` (`id1`),
   KEY `id2_idx` (`id2`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-SET FOREIGN_KEY_CHECKS=1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-12-31 22:09:51
